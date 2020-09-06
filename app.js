4d177170b008d9854ab2586dce9077d04e4f0dd6
Jimmy Wallener <jimmy.wallener@protonmail.com>
2020-09-06 11:34:27 +0200

    added comments

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
+/*
+School assignment designed to use:
+Self invoked anonymous function
+Custom Events
+AddEventListeners to handle Custome Events.
+Global Variables
+&& conditionals
+
+*/
+
+// Global Variable
 const main = document.querySelector(".main");
 
+// Self invoked anonymous function
 (function () {
+  // global varible within function
   const button = document.querySelector(".changes");
   const colorBox = document.querySelector(".colorbox");
   const textBox = document.querySelector(".textbox");
@@ -8,14 +21,16 @@ const main = document.querySelector(".main");
   let mainColor = 0;
   let boxColor = 0;
 
+  // First addeventlistener for customevent
   document.addEventListener("change-color", (e) => {
     main.style.backgroundColor = e.detail.main;
     colorBox.style.backgroundColor = e.detail.colorbox;
   });
+  // second addeventlistener for customevent
   document.addEventListener("change-color", (e) => {
     textBox.textContent = "Current HexColor: " + e.detail.colorbox;
   });
-
+  // changes color in divs on click through customevent
   button.addEventListener("click", () => {
     mainColor = colorChange();
     boxColor = colorChange();
@@ -28,9 +43,23 @@ const main = document.querySelector(".main");
     });
     document.dispatchEvent(event);
   });
+  // resets values through customEvent
   resetBtn.addEventListener("click", () => {
     mainColor && resetColor();
   });
+
+  // Functions here
+
+  // randomizes color hex
+  function colorChange() {
+    const index = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += index[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  // resets palett through CustomEvent
   function resetColor() {
     mainColor = "#000000";
     boxColor = "#000000";
@@ -41,15 +70,5 @@ const main = document.querySelector(".main");
       },
     });
     document.dispatchEvent(event);
-  }
-
-  // Functions here
-  function colorChange() {
-    const index = "0123456789ABCDEF";
-    let color = "#";
-    for (let i = 0; i < 6; i++) {
-      color += index[Math.floor(Math.random() * 16)];
-    }
-    return color;
   }
 })();
